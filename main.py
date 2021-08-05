11f4bb9dcb5906f3bc19dd758d0a14fd6c35f17d
SkyLex <86004812+SkyLexDev@users.noreply.github.com>
2021-08-05 18:31:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+```py
+import random, string
+
+amount = int(input('Üretilecek kod sayısını gir: '))
+value = 1
+while value <= amount:
+    code = "https://discord.gift/" + ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+    f = open('ghostcodes.txt', "a+")
+    f.write(f'{code}\n')
+    f.close()
+    print(f'[ÜRETİLDİ] {code}')
+    value += 1
+    ```
